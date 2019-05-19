c152c0dd56c3f24c2162624db2cf4fdca74a13a0
jonskoberne <jonchisko@gmail.com>
2019-05-19 22:02:11 +0200

    Initial commit

---
+++ Query.py
@@ -0,0 +1,34 @@
+# imports
+
+
+
+
+# class
+
+
+class QueryMe:
+
+
+
+    def __init__(self):
+        pass
+
+
+
+    def find_queries(self, queries_string):
+        queries_list = queries_string.split(" ")
+
+
+
+
+
+
+        
+
+
+
+
+if __name__ == '__main__':
+    pass
+
+
